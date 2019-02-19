0694684c4547cc6e1760726b105e408e797f7d61
Dan Fyfe <fyfedaniel@gmail.com>
2019-02-19 21:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,31 +5,33 @@ function takeANumber(currentLine, newCustomer){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length){
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
-    return "There is nobody waiting to be served!"
+    return 'There is nobody waiting to be served!';
   }
 }
 
 function currentLine(katzDeliLine){
-  var customer = "";
-  var comma = "";
-  var i;
+  var i = '';
+  var customer = '';
+  var comma = '';
+  
   function addComma(i){
-    if (i < katzDeliLine.length-1){
-      comma = ", ";
+    if(i < katzDeliLine.length -1){
+      comma = ', ';
       return comma;
     } else {
-      return "";
+      return '';
     }
   }
-  if (katzDeliLine.length){
-    for (i = 0; i < katzDeliLine.length; i++){
-      customer += `${i + 1}. ${katzDeliLine[i]}${addComma(i)}`;
-    }
-      return `The line is currently: ${customer}`;
-    } else {
-      return "The line is currently empty.";
-    }
-}
   
+  
+  if(katzDeliLine.length){
+    for (i = 0; i < katzDeliLine.length; i++){
+      customer += `${i +1}. ${katzDeliLine[i]}${addComma(i)}`;
+    }
+    return `The line is currently: ${customer}`;
+  } else {
+    return 'The line is currently empty.';
+  }
+}
